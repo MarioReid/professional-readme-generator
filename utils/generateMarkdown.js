c09dcb7809c1470b4097d994851a70661497542e
Mario Reid <mario.reid83@gmail.com>
2021-01-09 15:24:52 -0500

    created function that returns licenselink on generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,11 +9,15 @@ function renderLicenseBadge(license) {
     return "";
   }
 }
-
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === "none") {
+    return "";
+  }else {
+    return ` - [License](#license)`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
