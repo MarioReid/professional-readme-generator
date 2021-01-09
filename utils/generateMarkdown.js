1c79072c4f882069b98d78a78eff2fb513fae46a
Mario Reid <mario.reid83@gmail.com>
2021-01-09 15:54:27 -0500

    worked on function to add license to readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -21,23 +21,13 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 function renderLicenseSection(license) {
   if (license === "MIT") {
     return `### License
   This page uses the [MIT](https://choosealicense.com/licenses/mit/) license.`;
-  } else if (license === "ISC") {
+  } else if (license === "Apache") {
     return `### License
-
   This page uses the [ISC](https://choosealicense.com/licenses/isc/) license.`;
-  } else if (license === "GNU GPLv3") {
-    return `### License
-  This page uses the [GNU GPLv3](https://choosealicense.com/licenses/gpl-3.0/) license.`;
-  } else if (license === "BSD 3-Clause") {
-    return `### License
-
-  This page uses the [BSD 3-Clause](https://choosealicense.com/licenses/bsd-3-clause/) license.`;
   } else if (license === "none") {
     return "";
   }
@@ -81,7 +71,9 @@ function generateMarkdown(data) {
 
   Find me on GitHub: [${data.username}](https://github.com/${data.username})
 
-  Email me with any questions: ${data.email}`;
-}
+  Email me with any questions: ${data.email}
 
+${renderLicenseSection(data.license)}
+`;
+}
 module.exports = generateMarkdown;
